fc05d27d0b846840540b08b3c9c07dd064a07626
Ridho <ridhosirunsurinta@gmail.com>
2021-10-20 18:30:05 +0800

    Edit Mutation

--- schema/schema.js
+++ schema/schema.js
@@ -108,6 +108,31 @@ const mutation = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    editUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        firstName: {
+          type: GraphQLString,
+        },
+        age: {
+          type: GraphQLInt,
+        },
+        companyId: {
+          type: GraphQLString,
+        },
+      },
+      resolve(parentValue, { id, firstName, age, companyId }) {
+        return axios.patch(`http://localhost:3000/users/${id}`, {
+          firstName,
+          age,
+          companyId,
+        })
+          .then((res) => res.data);
+      },
+    },
     deleteUser: {
       type: UserType,
       args: {
@@ -119,7 +144,7 @@ const mutation = new GraphQLObjectType({
         return axios.delete(`http://localhost:3000/users/${id}`)
           .then((res) => res.data);
       },
-    }
+    },
   },
 });
 
