5346a732641db5e6e8ecb955e247e64562e5ba60
Ridho <ridhosirunsurinta@gmail.com>
2021-10-20 18:10:59 +0800

    Delete Mutation

--- schema/schema.js
+++ schema/schema.js
@@ -108,6 +108,18 @@ const mutation = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/users/${id}`)
+          .then((res) => res.data);
+      },
+    }
   },
 });
 
