e0cf0254262b727156f5352ec836508e9cdd60aa
Ridho <ridhosirunsurinta@gmail.com>
2021-10-20 14:53:16 +0800

    A Realistic Data Source

--- schema/schema.js
+++ schema/schema.js
@@ -7,19 +7,6 @@ const {
 } = graphql;
 const _ = require('lodash');
 
-const users = [
-  {
-    id: '23',
-    firstName: 'Bill',
-    age: 20,
-  },
-  {
-    id: '47',
-    firstName: 'Samantha',
-    age: 19,
-  },
-];
-
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
