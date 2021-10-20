301a5fda780133dbe225a829cc24b29e77559481
Ridho <ridhosirunsurinta@gmail.com>
2021-10-20 11:13:20 +0800

    Registering GraphQL with Express

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP; // OR -> const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
 
-app.listen(4000, () => {
+app.use('/graphiql', expressGraphQL({ // OR -> app.use('/graphiql', graphqlHTTP({
+  graphiql: true,
+}));
+
+app.listen(1234, () => {
   console.log('Listening...');
 });
